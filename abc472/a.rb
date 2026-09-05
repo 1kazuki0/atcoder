b33c51fca99fd1ca4a3e6af864ea885a3be844b5
かずき <ka10fukuda28@icloud.com>
2026-09-05 10:00:22 +0900

    docs: abc472 a 回答

---
+++ abc472/a.rb
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc473/tasks/abc473_a
+# 2026/9/4 結果 AC
+
+word = gets.chomp
+result = word.chars.map do |i|
+  if i == "A"
+    "A"
+  else
+    "."
+  end
+end
+
+puts result.join
