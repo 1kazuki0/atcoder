442013275b5fb9d2d807e9a3a9796260bbff1ad0
かずき <ka10fukuda28@icloud.com>
2026-09-08 08:47:08 +0900

    docs: abc471 b 解説

--- abc471/a.rb
+++ abc471/a.rb
@@ -14,3 +14,21 @@ else
   puts "Nein"
 end
 
+# 正解
+a, b = gets.split.map(&:to_i)
+if a + b == 9
+  puts "Nine"
+elsif a - b == 9
+  puts "Nine"
+elsif a * b == 9
+  puts "Nine"
+elsif a / b == 9 && a % b == 0
+  puts "Nine"
+else
+  puts "Nein"
+end
+
+# 解説
+# 割り算の場合、割り切れなくても小数点を切り落としてしまうので、
+# 例えば、19 / 2 の答えは9になってしまう。
+# 割り切れるかどうかを確かめるために a % b == 0を条件として増やす。
